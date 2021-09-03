48850a8f5ce78769d798b7aeba33f98164548512
Claudren <89350724+Claudren@users.noreply.github.com>
2021-09-03 20:05:57 +0300

    InFile tests

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using _2021._08._21_Exam;
+using System.IO;
+using System;
 
 namespace TestProject1
 {
@@ -26,7 +28,7 @@ namespace TestProject1
         }
 
         [Test]
-        public void AllWordsIn2File()
+        public void AllWordsIn2Files()
         {
             Dictionary dictionary = new Dictionary("testingTexts2");
             dictionary.CreateDictionary();
@@ -67,12 +69,134 @@ namespace TestProject1
             else
                 Assert.Fail();
         }
+
+        [Test]
+        public void IfEmty()
+        {
+            Dictionary dictionary = new Dictionary("testingTexts3");
+            dictionary.CreateDictionary();
+
+            if (!(dictionary.dictionary.Count == 0))
+                Assert.Fail();
+        }
+
+        [Test]
+        public void Sort()
+        {
+            Dictionary dictionary = new Dictionary("testingTexts");
+            dictionary.CreateDictionary();
+            dictionary.Sort();
+
+            if (dictionary.dictionary.Keys.Count != 4)
+                Assert.Fail();
+
+            int prev = int.MaxValue;
+
+            foreach (var item in dictionary.dictionary)
+            {
+                if (item.Value > prev)
+                    Assert.Fail();
+                prev = item.Value;
+            }
+        }
     }
 
 
     [TestFixture]
     public class InFile
     {
+        [Test]
+        public void AllWords()
+        {
+            Dictionary dictionary = new Dictionary("testingTexts");
+            dictionary.CreateDictionary();
+            dictionary.Sort();
+            dictionary.Save("test.txt");
 
+            StreamReader reader = new StreamReader("test.txt");
+            while (!reader.EndOfStream)
+            {
+                if (!dictionary.dictionary.ContainsKey(reader.ReadLine().Split(' ')[0]))
+                    Assert.Fail();
+            }
+            reader.Close();
+        }
+
+        [Test]
+        public void AllWordsIn2Files()
+        {
+            Dictionary dictionary = new Dictionary("testingTexts2");
+            dictionary.CreateDictionary();
+            dictionary.Sort();
+            dictionary.Save("test.txt");
+
+            StreamReader reader = new StreamReader("test.txt"); 
+            while (!reader.EndOfStream)
+            {
+                if (!dictionary.dictionary.ContainsKey(reader.ReadLine().Split(' ')[0]))
+                    Assert.Fail();
+            }
+            reader.Close();
+        }
+
+        [Test]
+        public void Counted()
+        {
+            Dictionary dictionary = new Dictionary("testingTexts");
+            dictionary.CreateDictionary();
+            dictionary.Sort();
+            dictionary.Save("test.txt");
+            int value, value2;
+            string key;
+            string[] tokens;
+
+            StreamReader reader = new StreamReader("test.txt"); 
+            while (!reader.EndOfStream)
+            {
+                tokens = reader.ReadLine().Split(' ');
+                key = tokens[0];
+                value = Convert.ToInt32(tokens[2]);
+                dictionary.dictionary.TryGetValue(key, out value2);
+                if (!dictionary.dictionary.ContainsKey(key) || value != value2)
+                    Assert.Fail();
+            }
+            reader.Close();
+        }
+
+        [Test]
+        public void IfEmty()
+        {
+            Dictionary dictionary = new Dictionary("testingTexts3");
+            dictionary.CreateDictionary();
+            dictionary.Sort();
+            dictionary.Save("test.txt");
+            FileInfo file = new FileInfo("test.txt");
+
+            if (!file.Exists || file.Length != 0)
+                Assert.Fail();
+        }
+
+        [Test]
+        public void Sort()
+        {
+            Dictionary dictionary = new Dictionary("testingTexts");
+            dictionary.CreateDictionary();
+            dictionary.Sort();
+            dictionary.Save("test.txt");
+
+            int prev = int.MaxValue;
+            int value;
+            StreamReader reader = new StreamReader("test.txt");
+            while (!reader.EndOfStream)
+            {
+                value = Convert.ToInt32(reader.ReadLine().Split(' ')[2]);
+                if (value > prev)
+                    Assert.Fail();
+                prev = value;
+            }
+            reader.Close();
+        }
     }
+
+    
 }
